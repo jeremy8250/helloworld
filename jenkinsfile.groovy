2d3cefd2d7407ea97a2bfe08f3e8fb878489070d
jeremy8250 <jeremy8250@gmail.com>
2020-06-11 21:05:08 +0800

    add dockerfile and jenkinsfile.groovy

---
+++ jenkinsfile.groovy
@@ -0,0 +1,47 @@
+stage('pull source code'){
+    node('master'){
+        git([url: 'https://github.com/jeremy8250/helloworld.git', branch: 'master'])
+    }
+}
+
+stage('maven compile & package'){
+    node('master'){
+        sh ". ~/.bash_profile"
+
+	def mvnHome = tool 'apache-maven-3.6.3'
+	env .PATH = "${mvnHome}/bin:${env.PATH}"
+	sh "mv target/hellowworld.war target/ROOT.war"
+    }
+}
+
+stage('clean docker environment'){
+    node('master'){
+        try{
+	    sh 'docker stop mytomcat'
+	}catch(exec){
+	    echo 'mytomcat container is not running!'
+        }
+
+	try{
+	    sh 'docker rm mytomcat'
+        }catch(exec){
+	    echo 'mytomcat container does not exist!'
+	}
+
+	try{
+	    sh 'docker rmi mytomcat'
+	}catch(exec){
+	    echo 'mytomcat images does not exist!'
+	}
+    }
+}
+
+stage('start docker container'){
+    node('master')
+	try{
+	    sh 'docker run --name mytomcat od -p 8089:8080 tomcat'
+	}catch(exec){
+	    echo 'Start docker images failed, please check the environment!'
+	}
+    }
+}
